977a5f92ccead77e9516c674f60b965e94206156
evidyasagar <42410939+evidyasagar@users.noreply.github.com>
2024-01-02 11:48:33 -0500

    imported linechart function from app_2 into index.js main file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App_2 from './App_2';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App_2 />
   </React.StrictMode>
 );
 
